252feac17f90df0b2829af9f62e5608a1e6cde41
Naoki Wayama <n.wayama0402@gmail.com>
2023-12-10 06:19:59 +0900

    [WIP] change: func

--- src/main.py
+++ src/main.py
@@ -4,4 +4,16 @@
 def main() -> None:
     """_summary_"""
     print("Hello world!")
-    print("Hellosss world!")
+
+
+# print("Hellosss world!")
+
+
+def add_int(a: int, b: int) -> int:
+    """_summary_"""
+    return a + b
+
+
+def add_float(a: float, b: float) -> float:
+    """_summary_"""
+    return a + b
